4ab942ca8705c00b4b7b3ba70e2e89e9d4adf5c9
Luke Woodward <luke.wdwrd@gmail.com>
2017-05-15 00:15:39 -0500

    added keys to info.plist, created options config for camera

--- components/imagebar.js
+++ components/imagebar.js
@@ -5,22 +5,43 @@ import {
   Text,
   TouchableHighlight,
   Image,
+  Alert,
 } from 'react-native'
 
 import React, { Component, PropTypes } from 'react';
 import styles from '../styles/styles';
+import ImagePicker from 'react-native-image-picker';
 
 
 export default class ImageBar extends Component {
+
+  _openCamera = () => {
+    const options = {
+      title: null,
+      cameraType: 'back',
+      mediaType: 'photo',
+      storageOptions: {
+        cameraRoll: true,
+      },
+    }
+
+    ImagePicker.launchCamera(options, (response)  => {
+      console.log(response)
+    })
+
+  }
+
+  _openImages = () => {
+  }
 
 
   render() {
     return (
       <View style={styles.imgBarCont}>
-        <TouchableHighlight onPress={this.thanksForPost}>
+        <TouchableHighlight underlayColor='white' onPress={this._openCamera}>
           <Image style={styles.img} source={require('../img/takepic.png')} />
         </TouchableHighlight>
-        <TouchableHighlight onPress={this.thanksForPost}>
+        <TouchableHighlight underlayColor='white' onPress={this._openImages}>
           <Image style={styles.img} source={require('../img/selectpicture.png')} />
         </TouchableHighlight>
       </View>
